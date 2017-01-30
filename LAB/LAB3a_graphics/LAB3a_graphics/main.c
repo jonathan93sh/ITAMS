db4e2610ee02f086e4cf0640f7682b7766bc848a
jonathan93sh <schouga4rd@gmail.com>
2017-01-30 15:05:50 +0100

    Graphic init

---
+++ LAB/LAB3a_graphics/LAB3a_graphics/main.c
@@ -0,0 +1,18 @@
+/*
+ * LAB3a_graphics.c
+ *
+ * Created: 30-01-2017 15:04:33
+ * Author : jonathan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
