c82bf0b13020764e5f78c21dd2da9ada4e0b2daa
jonathan93sh <schouga4rd@gmail.com>
2017-02-06 14:00:01 +0100

    HHboot init

---
+++ LAB/LAB06_HHBoot/uart.h
@@ -0,0 +1,9 @@
+/**************************************
+* "uart.h":                           *
+* Header file for Mega32 UART driver. *
+* Henning Hargaard, 29.1.2015          *
+***************************************/ 
+void InitUART();
+char ReadChar();
+void SendChar(char Ch);
+/**************************************/
